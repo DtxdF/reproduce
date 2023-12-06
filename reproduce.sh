b7b966e612a0a035afd18fa89fc586283fee2468
Jesús Daniel Colmenares Oviedo <DtxdF@disroot.org>
2023-12-06 15:15:10 -0400

    List projects using ls(1) instead of find(1)

--- reproduce.sh
+++ reproduce.sh
@@ -233,7 +233,7 @@ main()
 
         total="$#"
     else
-        PROJECTS=`cd -- "${PROJECTSDIR}"; find . -mindepth 1 -maxdepth 1 -type d | cut -c3-`
+        PROJECTS=`ls -- "${PROJECTSDIR}"` || exit $?
 
         if [ -z "${PROJECTS}" ]; then
             err "There are no projects to build."
